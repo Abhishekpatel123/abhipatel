6ec20defc420401fa996b0e363b746221a69e378
Abhishekpatel123 <68077909+Abhishekpatel123@users.noreply.github.com>
2020-08-29 11:33:55 +0530

    Update and rename co1.js to script.js

--- courseraproject4/script.js
+++ courseraproject4/script.js
@@ -4,7 +4,7 @@ function Goodby() {
     for (var i = 0; i < arr.length; i++) {
         var name = arr[i].charAt(0);
         if (name == 'j' || name == 'J') {
-            console.log("Goodby " + arr[i]);
+            console.log("Good by " + arr[i]);
 
 
         }
